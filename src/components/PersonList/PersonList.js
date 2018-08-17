8feeb6cdae106e847343980ab09d3d6c5a0788e7
Alisha Virani <alisha.virani@bookmyshow.com>
2018-08-17 18:14:57 +0530

    Added lifecycle methods on components

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/person'
 
-const PersonList = (props) => props.persons.map((person, personIndex) => {
-    return (
-      <Person
-        click={() => {props.clicked(personIndex)}}
-        name={person.name} 
-        age={person.age}
-        key={person.id}
-        changed={(event) => {props.changed(event, person.id)}}/> 
-    )
-})
+class PersonList extends Component {
+    constructor(props) {
+        super(props);
+        console.log("PersonList.js//Inside constructor!!!!!");
+      }
+    
+      componentWillMount = () => {
+        console.log("PersonList.js//Inside componentWillMount!!!!!");
+      }
+      
+      componentDidMount = () => {
+        console.log("PersonList.js//Inside componentDidMount!!!!!");
+      }
+      
+    //Can use the below syntax as we are not referring to the 'this' keyword and no issues will be arised
+    //   componentDidMount() {
+    //     console.log("PersonList.js//Inside componentDidMount!!!!!");
+    //   }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("UPDATE:PersonList.js//Inside componentWillReceiveProps!!!!!", nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("UPDATE:PersonList.js//Inside shouldComponentUpdate!!!!!", nextProps, nextState);
+        return nextProps.person !== this.props.persons;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("UPDATE:PersonList.js//Inside componentWillUpdate!!!!!", nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log("UPDATE:PersonList.js//Inside componentDidUpdate!!!!!");
+    }
+
+    render () {
+        console.log("PersonList.js//Inside render!!!!!");
+        return this.props.persons.map((person, personIndex) => {
+            return <Person
+                click={() => {this.props.clicked(personIndex)}}
+                name={person.name} 
+                age={person.age}
+                key={person.id}
+                changed={(event) => {this.props.changed(event, person.id)}}/> 
+        }) 
+    }
+} 
 
 export default PersonList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,13 +5,38 @@ import Cockpit from '../components/cockpit/Cockpit'
 
 class App extends Component {
 
-  state = {
-    persons: [
-      {id:'xgfcvbhnj',name: "Alisha", age: 22},
-      {id: 'cgvhbjnnm',name: "Ali", age: 22},
-      {id: 'ghkjlhjkm',name: "Sha", age: 22}
-    ],
-    showPersons: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        {id:'xgfcvbhnj',name: "Alisha", age: 22},
+        {id: 'cgvhbjnnm',name: "Ali", age: 22},
+        {id: 'ghkjlhjkm',name: "Sha", age: 22}
+      ],
+      showPersons: false
+    }
+    console.log("App.js//Inside constructor!!!!!");
+  }
+
+  componentWillMount = () => {
+    console.log("App.js//Inside componentWillMount!!!!!");
+  }
+  
+  componentDidMount = () => {
+    console.log("App.js//Inside componentDidMount!!!!!");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("UPDATE:App.js//Inside shouldComponentUpdate!!!!!", nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("UPDATE:App.js//Inside componentWillUpdate!!!!!", nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log("UPDATE:App.js//Inside componentDidUpdate!!!!!");
   }
 
   nameChangedHandler = (event, personId) => {
@@ -44,7 +69,7 @@ class App extends Component {
   }
 
   render() {
-
+    console.log("App.js//Inside render!!!!");
     let persons = null;
 
     if (this.state.showPersons) {
